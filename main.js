7710435f5e5dc61118045d75a1402c99eab88aab
Flexx <juandavidx40@gmail.com>
2022-10-26 15:31:42 -0500

    Update js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const $form = document.getElementById("form");
+const $buttonMailto = document.getElementById("trucazo");
+
+$form.addEventListener("submit", function handleSubmit(event){
+    event.preventDefault()
+
+    const form = new FormData(this)
+
+    $buttonMailto.setAttribute("href", `mailto:juandavidx40@gmail.com?subject=${form.get("name")} ${form.get("email")}&body=${form.get("message")}`);
+    $buttonMailto.click()
+});
